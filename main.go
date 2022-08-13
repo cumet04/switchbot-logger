5e7055538d12a6e99cae8638e51ff91e2288ab8a
cumet04 <medalhkr@gmail.com>
2022-08-14 00:37:31 +0900

    parse

--- main.go
+++ main.go
@@ -5,15 +5,69 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	elog := log.New(os.Stderr, "", log.LstdFlags)
+
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		fmt.Println(s.Text())
+		line := s.Text()
+		structs, err := parseLine(line)
+		if err != nil {
+			elog.Printf("failed to parse line; %s", err)
+			elog.Printf("  given line: %s", line)
+			continue
+		}
+
+		for _, s := range structs {
+			// ここでaddressを見て具体処理に分岐する
+			fmt.Println(s)
+		}
 	}
 
 	if s.Err() != nil {
 		log.Fatal(s.Err())
 	}
+}
+
+type AdStructure struct {
+	DeviceAddress string
+	AdType        int
+	Data          string
+}
+
+func parseLine(line string) ([]AdStructure, error) {
+	tokens := strings.Split(line, "\t")
+
+	if len(tokens) == 0 {
+		return nil, fmt.Errorf("given input has no tokens")
+	}
+	addr := tokens[0]
+
+	// FIXME: "3stringごと"みたいなところもうちょいいい感じに書けないか
+	rest := tokens[1:]
+	if len(rest)%3 != 0 {
+		return nil, fmt.Errorf("invalid token count")
+	}
+	var ads []AdStructure
+	for ; len(rest) > 0; rest = rest[3:] {
+		rawAdType := rest[0]
+		// rest[1] はdescriptionだが、利用しないため読み取らずここで捨てる。refs scanner.py
+		rawData := rest[2]
+
+		t, err := strconv.Atoi(rawAdType)
+		if err != nil {
+			return nil, fmt.Errorf("given AdType isn't number: adtype=%s", rawAdType)
+		}
+		ads = append(ads, AdStructure{
+			DeviceAddress: addr,
+			AdType:        t,
+			Data:          rawData,
+		})
+	}
+
+	return ads, nil
 }
--- scanner.py
+++ scanner.py
@@ -5,6 +5,9 @@ class ScanDelegate(DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         results = [dev.addr]
         for (adtype, desc, value) in dev.getScanData():
+            # descはbluepyにハードコードされたテキストのため、受信した信号としての情報量は無い（adtypeと同値）
+            # そのため出力する必要性はあまり無いが、scannerの範疇では取得できるデータは全部投げておき、利用有無はアプリケーション側で判断する
+            # refs https://github.com/IanHarvey/bluepy/blob/v/1.3.0/bluepy/btle.py#L659
             results += [str(adtype), desc, value]
         print("\t".join(results), flush=True)
 

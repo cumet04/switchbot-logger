819eb3a73584a5d3a2cae9c73d5ecf4291c2b96c
cumet04 <medalhkr@gmail.com>
2022-08-21 10:14:55 +0900

    プラグミニのparseテスト

--- main_test.go
+++ main_test.go
@@ -100,6 +100,35 @@ func Test_parseMeterDataはServiceData以外のStructureにはnilを返す(t *te
 	}
 }
 
+func Test_parsePlugData(t *testing.T) {
+	records, err := parsePlugData(AdStructure{
+		DeviceAddress: "60:55:f9:35:99:ff", // DataにAddressが含まれるためxyでマスクするとhex.Decodeが失敗するのでffでマスク
+		AdType:        255,
+		Data:          "69096055f93599ff128010230085",
+	})
+
+	if err != nil {
+		t.Errorf("want no err, but got: %v", err)
+	}
+
+	wants := []Record{
+		{
+			DeviceId: "60:55:f9:35:99:ff",
+			Type:     "PowerOn",
+			Value:    1,
+		},
+		{
+			DeviceId: "60:55:f9:35:99:ff",
+			Type:     "Load",
+			Value:    13.3,
+		},
+	}
+
+	if !containExactly(wants, records) {
+		t.Errorf("want %v, but got %v", wants, records)
+	}
+}
+
 func containExactly[T comparable](as []T, bs []T) bool {
 	if len(as) != len(bs) {
 		return false

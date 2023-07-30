fcce88c82ac61544adfeee169f2355325aa414a1
inomoto <medalhkr@gmail.com>
2023-07-30 19:28:17 +0900

    秘匿情報をsynth時ではなくplan/apply時に参照するようにする
    そうするとsynth結果をリポジトリに置けたり、synthするだけなら秘匿情報を参照しなくてよくなる（CIでのsynthがやりやすい）

--- infra/src/baseConstruct.ts
+++ infra/src/baseConstruct.ts
@@ -1,22 +1,30 @@
+import {TerraformVariable} from 'cdktf';
 import {Construct} from 'constructs/lib/construct';
+
+export type AppContext = {
+  env: EnvType;
+  gcpProjectId: TerraformVariable;
+  gcpLocation: string;
+};
 
 export class BaseConstruct extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
   }
 
-  get env(): EnvType {
-    const v = this.node.getContext('env') as string;
-    if (v !== 'production' && v !== 'development')
-      throw new Error('invalid env');
-    return v;
+  get env() {
+    return this.appContext.env;
   }
 
   get projectId() {
-    return this.node.getContext('gcp_project_id') as string;
+    return this.appContext.gcpProjectId;
   }
 
   get gcpLocation() {
-    return this.node.getContext('gcp_location') as string;
+    return this.appContext.gcpLocation;
+  }
+
+  private get appContext(): AppContext {
+    return this.node.getContext('appContext') as AppContext;
   }
 }

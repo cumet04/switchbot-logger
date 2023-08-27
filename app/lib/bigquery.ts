48b49f46072c9c623ee34156ea751c19849c9afb
inomoto <medalhkr@gmail.com>
2023-08-27 22:54:59 +0900

    bigquery書き込み

--- app/lib/bigquery.ts
+++ app/lib/bigquery.ts
@@ -1,7 +1,28 @@
-import { setTimeout } from "timers/promises";
+import { BigQuery } from "@google-cloud/bigquery";
 
-export async function Record() {
-  const res = await setTimeout(500, "result");
-  throw new Error("Hoge");
-  return;
+type Record = {
+  Time: Date;
+  DeviceId: string; // MACアドレス。現在は小文字
+  Type: "Battery" | "Temperature" | "Humidity" | "PowerOn" | "Load";
+  Value: number;
+};
+
+export async function Record(
+  projectId: string,
+  dataset: string,
+  table: string,
+  data: Record[]
+) {
+  const bigquery = new BigQuery();
+  bigquery
+    .dataset(dataset, { projectId })
+    .table(table)
+    .insert(
+      data.map((d) => ({
+        Time: bigquery.timestamp(d.Time),
+        DeviceId: d.DeviceId,
+        Type: d.Type,
+        Value: d.Value,
+      }))
+    );
 }
--- app/lib/bigquery_test.ts
+++ app/lib/bigquery_test.ts
@@ -2,8 +2,22 @@ import assert from "node:assert";
 import test from "node:test";
 import { Record } from "./bigquery";
 
+// BigQueryエミュレータを用意するのは面倒なので、devの実環境にinsertして結果を目視するかたちで検証する
 test("it success", () => {
   assert.doesNotReject(async () => {
-    await Record();
+    await Record(process.env.PROJECT_ID_DEVELOPMENT!, "switchbot", "metrics", [
+      {
+        Time: new Date(),
+        DeviceId: "test",
+        Type: "Battery",
+        Value: 100,
+      },
+      {
+        Time: new Date(),
+        DeviceId: "test",
+        Type: "Temperature",
+        Value: 20.7,
+      },
+    ]);
   });
 });

d4b384255901198d798a7b66a5d6776135109c5c
inomoto <medalhkr@gmail.com>
2023-09-27 15:33:22 +0900

    型間違ってた

--- app/lib/record.d.ts
+++ app/lib/record.d.ts
@@ -1,10 +1,10 @@
 type MacAddress = string & { readonly _brand: unique symbol }; // MACアドレス。現在は小文字
 type DeviceId = string & { readonly _brand: unique symbol }; // SwitchBotで管理されるデバイスID。hex大文字
-type DeviceType = "Battery" | "Temperature" | "Humidity" | "PowerOn" | "Load";
+type RecordType = "Battery" | "Temperature" | "Humidity" | "PowerOn" | "Load";
 
 type SensorRecord = {
   Time: Date;
   DeviceId: MacAddress; // 現時点ではDeviceIdとしてMACアドレスが入っている
-  Type: DeviceType;
+  Type: RecordType;
   Value: number;
 };
--- app/lib/switchbot.ts
+++ app/lib/switchbot.ts
@@ -1,6 +1,16 @@
 import crypto from "crypto";
 import { env } from "./envvars";
 
+export const DeviceTypes = [
+  "Plug Mini (US)",
+  "Plug Mini (JP)",
+  "Motion Sensor",
+  "Meter",
+  "WoIOSensor",
+  "Ceiling Light",
+] as const;
+type DeviceType = (typeof DeviceTypes)[number];
+
 // 現時点で使う予定のあるステータスのみ定義。infraredRemoteListやhubDeviceIdは無視。
 type Device = {
   deviceId: DeviceId;

ddf72cf1b112651fd9c5931ca3e5c65d6eda5c6f
inomoto <medalhkr@gmail.com>
2024-01-28 20:04:36 +0900

    ランタイム環境変数読み込みのパッチを削除
    Next14で直ったっぽい

--- app/lib/envvars.ts
+++ app/lib/envvars.ts
@@ -1,3 +1,8 @@
+// MEMO: 起動時に環境変数の存在確認を行っていない。
+// standaloneモードでinstrumentationが動くようになったら or ヘルスチェックエンドポイントを作ったら、
+// そこらあたりで存在確認をするようにしたい
+// refs https://github.com/vercel/next.js/issues/49897
+
 const keys = [
   "PROJECT_ID",
   "AUTH_PATH",

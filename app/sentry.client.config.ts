582f40edea282abf8c57983783f6f6dc40bc00c5
inomoto <medalhkr@gmail.com>
2024-03-24 16:19:43 +0900

    環境取得をやりやすいようにする

--- app/sentry.client.config.ts
+++ app/sentry.client.config.ts
@@ -1,8 +1,11 @@
 import * as Sentry from "@sentry/nextjs";
+import { appenv } from "./lib/envvars";
+
+const environment = appenv();
 
 Sentry.init({
   dsn: "https://53f5a903731e07e39ea32248e65c6a22@o4506720064962560.ingest.sentry.io/4506819121709056",
-  environment: process.env.NEXT_PUBLIC_APP_ENV ?? "local",
+  environment,
 
   tracesSampleRate: 1,
   replaysOnErrorSampleRate: 1.0,

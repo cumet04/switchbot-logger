e931f62d45888c315aaaadcf797f0ce4582808f6
inomoto <medalhkr@gmail.com>
2024-03-24 17:01:13 +0900

    実行都度serveしてやりづらいのを変更

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -9,8 +9,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
